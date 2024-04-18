bb69265d01d4b3dbf3bf1c6895c5f84f5f0df086
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-04-19 00:55:35 +0530

    adding toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,24 @@
-import { BrowserRouter , Routes, Route } from "react-router-dom"
-import Home from "./Pages/Home"
-import User from "./Pages/User"
-import PostJob from "./Pages/PostJob"
-import MyJobs from "./Pages/MyJobs"
-import UpdateJob from "./Pages/UpdateJob"
-import Navbar from "./Components/Navbar"
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import User from "./Pages/User";
+import PostJob from "./Pages/PostJob";
+import MyJobs from "./Pages/MyJobs";
+import UpdateJob from "./Pages/UpdateJob";
+import Navbar from "./Components/Navbar";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
-   <BrowserRouter>
-    <Navbar />
-   <Routes>
-    <Route index element={<Home/>}/>
-    <Route path="/user" element={<User/>}/>
-    <Route path="/post-job" element={<PostJob/>}/>
-    <Route path="/my-jobs" element={<MyJobs/>}/>
-    <Route path="/updatejob/:id" element={<UpdateJob/>}/>
-  
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/post-job" element={<PostJob />} />
+        <Route path="/my-jobs" element={<MyJobs />} />
+        <Route path="/updatejob/:id" element={<UpdateJob />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
